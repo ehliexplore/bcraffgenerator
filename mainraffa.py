ffeaceba918d508803105ef80ee3285a122d36c7
Luis Guilherme <48739895+ehliexplore@users.noreply.github.com>
2023-03-19 23:55:46 -0300

    commit inicial

---
+++ mainraffa.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import random
+from phrases import lyrics_raffa
+
+app = Flask(__name__)
+
+phrases = lyrics_raffa()
+
+@app.route('/')
+def index():
+    random_phrases = random.choice(phrases)
+    return render_template('index.html', quote=random_phrases)
+
+
+@app.route('/')
+def reload():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
